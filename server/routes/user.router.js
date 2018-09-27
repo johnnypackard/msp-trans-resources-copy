f83ea7e3e6991bf065a2d35585b0293e930c6285
Johnny Packard <packard81@me.com>
2018-09-27 10:16:25 -0500

    adding querytext to user router for posting registration

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -16,15 +16,29 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {
-  console.log('req: ', req.body);
-  
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
-
-  const queryText = 'INSERT INTO person (username, password) VALUES ($1, $2) RETURNING id';
-  pool.query(queryText, [username, password])
-    .then(() => { res.sendStatus(201); })
-    .catch((err) => { next(err); });
+  const type = "user";
+  const email = req.body.email;
+  const validated = true;
+  const bio = req.body.bio;
+  const contact_info = req.body.contact_info;
+  const queryText = `INSERT INTO users (username, hash, type, email, validated) VALUES ($1, $2, $3, $4, $5) RETURNING id`;
+  pool.query(queryText, [username, password, type, email, validated])
+    .then((result) => { 
+      const user_id = result.rows[0].id;
+      const profileQueryText = `INSERT INTO profiles (user_id, bio, contact_info) VALUES ($1, $2, $3)`
+      pool.query(profileQueryText, [user_id, bio, contact_info]).then(() => {
+        console.log('user profile created');
+        res.sendStatus(201);
+      }).catch((error) => {
+        console.log('error creating user profile', error);
+        res.sendStatus(500);
+      })
+    })
+    .catch((err) => { 
+      next(err); 
+    });
 });
 
 // Handles login form authenticate/login POST
