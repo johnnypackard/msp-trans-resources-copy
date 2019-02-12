1a4fdccd3b39905c5a3878aeef3cceb1d748f84c
Johnny Packard <37680772+johnnypackard@users.noreply.github.com>
2019-02-12 14:11:29 -0600

    adding queue-it to page for testing

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import reducer from './redux/reducers';
 import App from './App';
 import rootSaga from './redux/sagas';
 
+// Queue-it test
+<script type="text/javascript" src="//static.queue-it.net/script/queueclient.min.js"></script>
+  <script 
+    data-queueit-c="johnnytest" 
+    type="text/javascript" 
+    src="//static.queue-it.net/script/queueconfigloader.min.js">
+</script>
+
 // Initializing to an empty object, but here is where you could
 // preload your redux state with initial values (from localStorage, perhaps)
 const preloadedState = {};
